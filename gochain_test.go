cf771cac6fe4e4323602ffea07e4737a05817dc0
Tyler King <tyler@ohmybrew.com>
2019-03-27 00:29:53 -0230

    Add a complete test

---
+++ gochain_test.go
@@ -0,0 +1,33 @@
+package gochain
+
+import (
+	"testing"
+
+	"github.com/ohmybrew/gochain/chain"
+	"github.com/ohmybrew/gochain/miner"
+)
+
+func TestWhole(t *testing.T) {
+	// Difficulty level, data for blocks, and new chain.
+	dif := 2
+	data := "Hello World"
+	c := chain.New()
+
+	// Genesis block and another block joined to it.
+	blk := miner.New(nil, dif, data)
+	blk2 := miner.New(blk, dif, data)
+
+	// Mine both blocks.
+	blk.Miner.Mine()
+	blk.Miner.GenerateHash(true)
+	blk2.Miner.Mine()
+	blk2.Miner.GenerateHash(true)
+
+	// Append the blocks.
+	c.Append(false, blk, blk2)
+
+	// Validate the chain
+	if !c.IsValid() {
+		t.Errorf("chain should be valid but returned invalid")
+	}
+}
